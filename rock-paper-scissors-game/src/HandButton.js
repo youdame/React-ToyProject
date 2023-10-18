606129ccd30369e5d334402009098d4ff5e33e2f
youdame <gmlwjd2400@naver.com>
2023-10-18 14:48:25 +0900

    Feat(rsp-game) : 클래스 네임 적용

--- rock-paper-scissors-game/src/HandButton.js
+++ rock-paper-scissors-game/src/HandButton.js
@@ -1,26 +1,15 @@
 import HandIcon from "./HandIcon";
 import purple from "./assets/purple.svg";
-const style = {
-  width: '166px',
-  height: '166px',
-  border: 'none',
-  outline: 'none',
-  textAlign: 'center',
-  cursor: 'pointer',
-  backgroundColor: 'transparent',
-  backgroundImage: `url(${purple})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  backgroundSize: 'contain'
-}
+import './HandButton.css'
+
 
 
 function HandButton({ value, onClick }) {
 
   const handleClick = () => onClick(value);
   return (
-  <button style = {style} onClick = {handleClick}>
-    <HandIcon value ={value}/>
+  <button className = 'HandButton' onClick = {handleClick}>
+    <HandIcon className = 'HandButton-icon' value ={value}/>
 
   </button>)
 }
--- rock-paper-scissors-game/src/HandIcon.js
+++ rock-paper-scissors-game/src/HandIcon.js
@@ -1,16 +1,17 @@
 import rock from "./assets/rock.svg";
 import scissor from "./assets/scissor.svg";
 import paper from "./assets/paper.svg";
-
+import "./HandButton"
 
 const IMAGES ={
   rock,
   scissor,
   paper
 }
-function HandIcon({value}) {
+function HandIcon({value, className}) {
   const src = IMAGES[value];
-  return <img src={src} alt= {value} />;
+
+  return <img src={src} className = {className}alt= {value} />;
 }
 
 export default HandIcon;
